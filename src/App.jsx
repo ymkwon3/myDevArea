6e3774a99f12eefd1c63f72f6154a350c63366fb
youngmin <ymkwon3@gnu.ac.kr>
2021-08-24 23:46:56 +0900

    npm package redownload

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import Footer from './components/Footer'
 import Header from './components/Header'
 import ReactDOM from 'react-dom';
 
-function Welcome(props) {
-  return <h1>어, {props.name}이 어서오고!</h1>;
+function WhoAmI(props) {
+  return <div>
+  <h2>이름: {props.name}</h2>
+  <h2>좋아하는 것: 게임을 좋아하는 것으로 추정</h2>
+  <h2>잘하는 것: 꾸준히 찾아보는 중...</h2>
+  </div>;
 }
 
 function App() {
   return (
     <div className="App flex-column-center" id="app">
-      <Welcome name="영민"/>
-      <Welcome name="잉민"/>
+      <WhoAmI name="권영민"/>
       <div className="flex-column-center footer">
         <Footer className="footer"/>
       </div>
